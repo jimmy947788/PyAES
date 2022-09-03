15c30a5889d14c0f3207502156297bdde7ba9c15
吉掰米 jimmy947788 <jimmy.wu@daedalus.cc>
2022-09-03 21:19:44 +0800

    修改模組資訊

--- setup.py
+++ setup.py
@@ -121,13 +121,13 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="cmake_example",
+    name="PyAES",
     version="0.0.1",
-    author="Dean Moldovan",
-    author_email="dean0x7d@gmail.com",
-    description="A test project using pybind11 and CMake",
+    author="Jimmy Wu",
+    author_email="jimmy.wu@daedalus.com.tw",
+    description="A AES lib using pybind11 and CMake",
     long_description="",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("PyAES")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
